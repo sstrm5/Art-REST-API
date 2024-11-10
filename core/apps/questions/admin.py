dfb0a0bc6e7b910bc1b788d07fc78f85c7917530
Mikhail Ovechkin <mpvglondon@vk.com>
2024-11-10 14:43:05 +0400

    added django nested admin

--- core/apps/questions/admin.py
+++ core/apps/questions/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+import nested_admin
 
 from core.apps.questions.models.attempts import Attempt
 
@@ -6,12 +7,6 @@ from .models.questions import Test, Question, Answer
 from .models.subjects import Subject
 
 # Register your models here.
-
-
-@admin.register(Test)
-class ProductAdmin1(admin.ModelAdmin):
-    list_display = ('id', 'title', 'subject', 'created_at',
-                    'question_count', 'description', 'is_visible')
 
 
 @admin.register(Question)
@@ -35,3 +30,24 @@ class ProductAdmin4(admin.ModelAdmin):
 class ProductAdmin5(admin.ModelAdmin):
     list_display = ('id', 'user', 'test', 'end_time',
                     'user_answers', 'total_score', 'attempt_number')
+
+
+# Or StackedInline for a different layout
+class AnswerInline(nested_admin.NestedTabularInline):
+    model = Answer
+    extra = 1  # Number of empty answer forms to display
+    show_change_link = True
+
+
+class QuestionInline(nested_admin.NestedTabularInline):
+    model = Question
+    extra = 1
+    show_change_link = True
+    inlines = (AnswerInline,)
+
+
+@admin.register(Test)
+class ProductAdmin1(nested_admin.NestedModelAdmin):
+    list_display = ('id', 'title', 'subject', 'created_at',
+                    'question_count', 'description', 'is_visible')
+    inlines = (QuestionInline,)
--- core/project/settings/main.py
+++ core/project/settings/main.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
     # third party
     "corsheaders",
+    'nested_admin',
 ]
 
 MIDDLEWARE = [
