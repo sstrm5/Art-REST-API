cc1562f3133209a25f4ff397bcd1f57c8e795875
Mikhail Ovechkin <mpvglondon@vk.com>
2024-11-11 16:05:48 +0400

    added nginx
    fix: storages.yaml
    
    fix: storages.yaml 2
    
    fix: nginx.conf
    
    fix: nginx.conf 2
    
    edited: nginx.conf

--- core/project/settings/main.py
+++ core/project/settings/main.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY', default='mysecretkey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
