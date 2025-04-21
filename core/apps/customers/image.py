f03c75cc1026ef7d878a72d36a667cdaff8fa8fe
Mikhail Ovechkin <hatedyoufromhello@yandex.ru>
2025-04-21 13:51:54 +0400

    reduced image compression

--- core/apps/customers/image.py
+++ core/apps/customers/image.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 
-def compress_image(path, extension, max_size=(200, 200)):
+def compress_image(path, extension, max_size=(1920, 1080)):
     image = Image.open(path)
     print(image.size)
     image.thumbnail(max_size)
